c029aa33a3e3e80694fe7afdc44184a60315cd63
DominikBieleckikontakt <dominikbielecki.kontakt@gmail.com>
2024-08-27 16:36:21 +0200

    profile controller ended

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-  public function index()
+  public function index($user)
   {
-    return view('home');
+    $user = User::find($user);
+    return view('home', [
+      'user' => $user,
+    ]);
   }
 }
