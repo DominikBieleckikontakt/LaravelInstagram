1b091ac1768cccde456afb8cba3e39d12b26fdc3
DominikBieleckikontakt <dominikbielecki.kontakt@gmail.com>
2024-08-29 13:28:53 +0200

    adding posts to db is now available

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+
+Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
+Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
